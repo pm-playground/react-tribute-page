26c21d1ba94d01827a64059ad45da78d16df6ec3
o0pmitev <pmitev89@gmail.com>
2020-06-02 12:54:13 +0200

    Updates

---
+++ precache-manifest.47023770c8c1dda216befee16d6a44c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507181ca810526dfda101abf8193183b",
+    "url": "/react-tribute-page/index.html"
+  },
+  {
+    "revision": "e594078ae710bd34737f",
+    "url": "/react-tribute-page/static/css/main.e7154fe3.chunk.css"
+  },
+  {
+    "revision": "2969db31a731296ac4fa",
+    "url": "/react-tribute-page/static/js/2.f5b4244a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tribute-page/static/js/2.f5b4244a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e594078ae710bd34737f",
+    "url": "/react-tribute-page/static/js/main.9bcdd912.chunk.js"
+  },
+  {
+    "revision": "af0f57ef20fa4274f351",
+    "url": "/react-tribute-page/static/js/runtime-main.c8883e77.js"
+  },
+  {
+    "revision": "4751bd6dc998bcb3b92a16283c74cc40",
+    "url": "/react-tribute-page/static/media/bass.4751bd6d.jpg"
+  },
+  {
+    "revision": "c965264b46017a0aede903a05b1919d9",
+    "url": "/react-tribute-page/static/media/guitar.c965264b.png"
+  },
+  {
+    "revision": "442e2e55d194900abafbaf9489afbcc6",
+    "url": "/react-tribute-page/static/media/root.442e2e55.jpg"
+  },
+  {
+    "revision": "8d8e9277a2c8c5b95b9eba474bcdbc22",
+    "url": "/react-tribute-page/static/media/vocal.8d8e9277.jpg"
+  }
+]);
